2b87c7c7ca002665a726df2475b672fa0a48420b
weijiexu-charlie <56103694+weijiexu-charlie@users.noreply.github.com>
2025-09-13 23:37:55 +0000

    Deploying to gh-pages from @ weijiexu-charlie/weijiexu-charlie.github.io@cf21316de08b5a5f3caf7c9b799d0a32d505c20d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
