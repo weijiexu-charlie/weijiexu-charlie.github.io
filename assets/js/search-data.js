b54c34b54f23ee4a6f197f388a18cbf42b307577
weijiexu-charlie <56103694+weijiexu-charlie@users.noreply.github.com>
2025-09-14 00:41:24 +0000

    Deploying to gh-pages from @ weijiexu-charlie/weijiexu-charlie.github.io@1dc2f7a8a1d2fcbe204a039b699c5f3a1fd71bf5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "dropdown-南北东西路",
+              title: "南北东西路",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/outandabout/";
+              },
+            },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
