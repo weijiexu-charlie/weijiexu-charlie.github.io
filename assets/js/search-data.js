a3407bcebbf902c6739313db72854f4fbb99a28f
weijiexu-charlie <56103694+weijiexu-charlie@users.noreply.github.com>
2025-09-14 00:45:30 +0000

    Deploying to gh-pages from @ weijiexu-charlie/weijiexu-charlie.github.io@019eba6041958aa4a75f713cfb72060964f9de57 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,35 +466,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%65%69%6A%69%65.%78%75@%75%63%69.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/weijiexu-charlie", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/0009-0008-1258-6930", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=UrPCWf8AAAAJ&hl=en", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/weijiexu_97", "_blank");
         },
       },{
       id: 'light-theme',
