9c28cb1d71d84a44eb8dd2ccf9b4e73c3b2aa958
weijiexu-charlie <56103694+weijiexu-charlie@users.noreply.github.com>
2025-09-14 00:31:34 +0000

    Deploying to gh-pages from @ weijiexu-charlie/weijiexu-charlie.github.io@856cb52cb291b0b44926092d38473dee9a6da85d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,12 +423,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -468,6 +463,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-密歇根湖",
+          title: '密歇根湖',
+          description: "LAKE MICHIGAN",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/lake_michigan/";
             },},{
         id: 'social-email',
         title: 'email',
