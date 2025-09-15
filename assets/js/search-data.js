cafb899bb675c386a97fb2a2a6757f584ee76292
weijiexu-charlie <56103694+weijiexu-charlie@users.noreply.github.com>
2025-09-15 07:00:11 +0000

    Deploying to gh-pages from @ weijiexu-charlie/weijiexu-charlie.github.io@f678942f9b44e3c53e700cf65ff5a5b11a67285f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
+  },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
